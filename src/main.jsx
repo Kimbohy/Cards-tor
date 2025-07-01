1d1f79af643444f669be76d9e21eab4e74096280
Kimbohy <lovabarwijons@gmail.com>
2025-07-01 19:03:59 +0300

    Refactor App component to implement routing and clean up styles

--- src/main.jsx
+++ src/main.jsx
@@ -1,35 +1,10 @@
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import React from "react";
 import ReactDOM from "react-dom/client";
-// import App from './App'
-import './assets/styles/index.css'
-import Product from "./components/Products";
-import Registration from "./components/Registration";
-import Cart from "./components/Cart";
-import SignIn from "./components/SingIn";
-
-const route = createBrowserRouter([
-  {
-    path: "/",
-    element: <Product />,
-  },
-  {
-    path: "/Registration",
-    element: <Registration />,
-  },
-  {
-    path: "/Cart",
-    element: <Cart />,
-  },
-  {
-    path: "/SignIn",
-    element: <SignIn />,
-  },
-]);
+import App from "./App";
+import "./assets/styles/index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    {/* <App /> */}
-    <RouterProvider router={route} />
+    <App />
   </React.StrictMode>
 );
